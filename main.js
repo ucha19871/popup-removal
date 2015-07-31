7dfdc7a757d54a1f56c3724c076d0de564d447b0
Ucha Chilachava <u.chilachava@webfox.guru>
2015-07-31 19:14:36 +0400

    regex

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 (function( $ ) {
 
-    var ads = ['forads','parent_popup','ad_overlay','toPopup','modFBpopup'];
+    var ads = [
+        'forads','parent_popup','ad_overlay','toPopup','modFBpopup','colorbox','cbox','hstp_',
+        'liderbet','modFB','popup','banner','fancybox','ads','fanback'
+    ];
 
     setTimeout( function() {
         $.each(ads, function( index, value ) {
-            var id = "#"+value;
+            //var id = "#"+value;
+            var id = "[id^='"+value+"']";
             if ($(id).length){
                 $(id).remove();
             }
