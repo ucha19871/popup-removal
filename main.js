ed36e4fa4d0e3fdee754ea4163d36040177461df
HuchMan <ucha19871@gmail.com>
2015-08-02 23:26:55 +0400

    setup remote json file to go

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 (function( $ ) {
-
-    var ads = [
-        'forads','parent_popup','ad_overlay','toPopup','modFBpopup','colorbox','cbox','hstp_',
-        'liderbet','modFB','popup','banner','fancybox','ads','fanback'
-    ];
-
-    setTimeout( function() {
-        $.each(ads, function( index, value ) {
-            //var id = "#"+value;
-            var id = "[id^='"+value+"']";
-            if ($(id).length){
-                $(id).remove();
-            }
-
-        });
-
-    },200);
-
+    $.ajax({
+        url: 'param.json',
+        dataType:'json',
+        type:'get',
+        success: function (data) {
+            var ads = data.ads;
+            setTimeout( function() {
+                $.each(ads, function( index, value ) {
+                    var id = "[id^='"+value+"']";
+                    //console.log($(id).length);
+                    if ($(id).length){
+                        $(id).remove();
+                    }
+                });
+            },100);
+        }
+    });
 })( jQuery );
